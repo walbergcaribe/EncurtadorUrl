3a83cbb5c70d65bb1aaefc3edf73cf8c1f738d28
unknown <admuser@QGMICRCE-N025.qgmic.com>
2022-01-14 22:55:21 -0300

    inclusao de sql scripts.

---
+++ encurtador-url/scripts/create-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE public.tb_url_encurtada
+(
+    id bigint NOT NULL DEFAULT nextval('tb_url_encurtada_id_seq'::regclass),
+    url_original character varying(500)  NOT NULL,
+    url_encurtada character varying(500) NOT NULL,
+    data date NOT NULL,
+    CONSTRAINT tb_url_encurtada_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE public.tb_url_encurtada
+    OWNER to postgres;
